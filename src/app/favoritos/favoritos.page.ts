d6e7fecf1d02cf59662d19006a7d4b941bdd13fb
andregabriel23 <andregabrielvalenza@gmail.com>
2021-01-15 13:13:23 -0500

    Mis cambios

--- src/app/favoritos/favoritos.page.ts
+++ src/app/favoritos/favoritos.page.ts
@@ -14,7 +14,7 @@ export class FavoritosPage implements OnInit {
   @ViewChild (IonSlides, { static: false }) slide: IonSlides;
   slideOptsOne = {
     initialSlide: 0,
-    slidesPerView: 3,
+    slidesPerView: 2,
     spaceBetween: 10,
   };
   profesionales: any [] = [];
